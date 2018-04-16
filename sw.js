5d0beadafb124801c04571ca3f3e3936599e78d4
Bence Skorka <skorka.bence@gmail.com>
2018-04-16 14:37:10 +0200

    generated image list

--- sw.js
+++ sw.js
@@ -12,26 +12,12 @@ self.importScripts('js/DataLists.js');
 const preCacheList = [
     "audio/click.mp3",
     "audio/music.mp3",
-    "audio/win.mp3",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/01_gettyimages-450207051_super_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/02_gettyimages-544346474_super_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/03_gettyimages-642010642_high_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/04_gettyimages-536057853_high_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/05_gettyimages-170349835_high_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/06_gettyimages-74076688_super_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/07_gettyimages-483771218_super_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/08_gettyimages-512735064_super_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/09_gettyimages-510014070_super_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/10_gettyimages-452517269_high_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/11_gettyimages-490639794_super_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/12_gettyimages-534484823_super_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/13_gettyimages-861470990_super_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/14_gettyimages-635731456_high_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/15_gettyimages-492003884_high_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/16_gettyimages-621545726_high_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/17_gettyimages-92415747_high_resized.jpg",
-    "https://csfcloud.blob.core.windows.net/resources/sudoku/18_gettyimages-672313436_super_resized.jpg"
+    "audio/win.mp3"
 ];
+
+for (let i = 0; i < BackgroundImageList.length; ++i) {
+    preCacheList.push(BackgroundImagePrefix + BackgroundImageList[i]);
+}
 
 function SWLog(txt) {
     console.log("[Sudoku Service Worker] " + txt);
